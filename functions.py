723f897f6d473b265a87d949b642e45a09d2d746
dan-rooney <danjrooney36@gmail.com>
2024-10-13 21:55:35 +0100

    Create custom preprocessing class

--- functions.py
+++ functions.py
@@ -102,39 +102,63 @@ def transform_raw_data(path_to_csv):
     df_expanded = df_expanded.sort_values(['accident_index_2', 'vehicle_subtype'])
     df_expanded = df_expanded.groupby('accident_index_2').nth(0)
 
+    # creating date and time features
+    df_expanded['time_period'] = (df_expanded.time.str.slice(start=0, stop=2).astype('int') // 4)
+    df_expanded['time_period'] = (df_expanded['time_period'] * 4).astype('str') + ':00 - ' + ((df_expanded['time_period'] + 1) * 4).astype('str') + ':00'
+
+    df_expanded['date'] = pd.to_datetime(df_expanded.date)
+    df_expanded['month'] = df_expanded['date'].dt.month
+    df_expanded['season'] = [
+        'spring' if month in [3, 4, 5]
+        else 'summer' if month in [6, 7, 8]
+        else 'autumn' if month in [9, 10, 11]
+        else 'winter'
+        for month
+        in df_expanded['month']
+    ]
+
+    df_expanded = df_expanded.drop(columns=['date', 'month'])
+
     return df_expanded
 
-def clean_transform_df(df, select_features):
+def clean_transform_df(df, select_features, encoded_cols_dict=None):
     # standardise missing values
     missing_values = ['Data missing or out of range', 'unknown (self reported)', np.nan, 'Unknown', 'Not known', 'Undefined']
     df = df.replace({i: "Missing" for i in missing_values})
 
-    df = df.drop(columns=[
+    drop_columns = [
         'accident_index_2',
         'lsoa_of_casualty', 'lsoa_of_driver', 
         'age_band_of_casualty', 'age_band_of_driver',
         'special_conditions_at_site', 'carriageway_hazards', 'skidding_and_overturning', 'hit_object_in_carriageway', 'hit_object_off_carriageway', 'journey_purpose_of_driver', 'vehicle_leaving_carriageway',
         'age_of_vehicle'
-    ])
+    ]
+    drop_columns = [col for col in drop_columns if col in df.columns]
+    if len(drop_columns) > 0:
+        df = df.drop(columns=drop_columns)
 
     # categorise speed_limit
-    df['speed_limit'] = df['speed_limit'].astype('object')
+    if 'speed_limit' in df.columns:
+        df['speed_limit'] = df['speed_limit'].astype('object')
 
     # convert numeric to continuous
-    df[['engine_capacity_cc', 'age_of_casualty', 'age_of_driver']] = df[['engine_capacity_cc', 'age_of_casualty', 'age_of_driver']].replace('Missing', np.nan).astype('float')
+    for col in ['engine_capacity_cc', 'age_of_casualty', 'age_of_driver']:
+        if col in df.columns:
+            df[col] = df[col].replace('Missing', np.nan).astype('float')
 
     missing = pd.DataFrame([i for i in zip(df.columns, df.dtypes, df.nunique(), 100 * ((df == "Missing") | (df.isnull())).mean()) if i[3] > 0], columns=['column', 'dtype', 'nunique', 'missing %']).sort_values("missing %")
 
     # categorical missing values imputed while keeping the category distributions of each variable the same
     categorical_missing = list(missing[missing['dtype'] == 'object']['column'])
-    df = df.replace("Missing", np.nan)
+    df = df.replace("Missing", np.nan).reset_index(drop=True)
 
     for col in categorical_missing:
         freq_dict = df[col].value_counts(normalize=True).to_dict()
         df[col] = df[col].fillna(pd.Series(np.random.choice(list(freq_dict.keys()), p=list(freq_dict.values()), size=len(df))))
 
     # does not make sense for propulsion_code to have a value for cyclists
-    df["propulsion_code"] = np.where(df["vehicle_type"] == "Pedal cycle", "Undefined", df["propulsion_code"])
+    if 'propulsion_code' in df.columns:
+        df["propulsion_code"] = np.where(df["vehicle_type"] == "Pedal cycle", "Undefined", df["propulsion_code"])
 
     # creating new vehicle_type column because some values of vehicle_type have no values for engine_capacity_cc -> using other types of vehicle that have the most similar engine size
     df["vehicle_type_2"] = [
@@ -149,69 +173,66 @@ def clean_transform_df(df, select_features):
     # imputing continuous variables
     # first imputation should capture most nulls
     # second imputation should capture any remaining nulls (remaining because there were no values in the lookup group)
-    impute_dict_1 = {
-        "engine_capacity_cc": ["vehicle_type_2"],
-        "age_of_casualty": ["casualty_severity"],
-        "age_of_driver": ["vehicle_type", "casualty_severity"]
-    }
-    impute_dict_2 = {
-        "engine_capacity_cc": ["vehicle_type_2"],
-        "age_of_casualty": ["casualty_severity"],
-        "age_of_driver": ["casualty_severity"]
+    impute_dict = {
+        "engine_capacity_cc": "vehicle_type_2",
+        "age_of_casualty": "towing_and_articulation",
+        "age_of_driver": "vehicle_type"
     }
 
-    def impute_continuous_vars(df, impute_dict):
-        for var in impute_dict:
-            lookup_df = df.groupby(impute_dict[var])[var].median()
-            df = pd.merge(df, lookup_df, how="left", on=impute_dict[var], suffixes=["", "_y"])
-            df[var] = np.where(df[var].isna(), df[var + '_y'], df[var])
-            df = df.drop(columns=[var + '_y'])
+    def impute_continuous_vars(df, impute_lookup):
+        for var in impute_lookup:
+            if var in df.columns:
+                if type(impute_lookup) == dict and impute_lookup[var] in df.columns:
+                    lookup_df = df.groupby(impute_lookup[var])[var].median()
+                    df = pd.merge(df, lookup_df, how="left", on=impute_lookup[var], suffixes=["", "_y"])
+                    df[var] = np.where(df[var].isna(), df[var + '_y'], df[var])
+                    df = df.drop(columns=[var + '_y'])
+                else:
+                    impute_value = df[~df[var].isna()][var].median()
+                    df[var] = df[var].replace(np.nan, impute_value)
 
         return df
 
-    df_1 = impute_continuous_vars(df, impute_dict_1)
-    df_2 = impute_continuous_vars(df_1, impute_dict_2)
+    df_1 = impute_continuous_vars(df, impute_dict)
+    df_2 = impute_continuous_vars(df_1, impute_dict.keys())
     df = df_2
 
     # "Pedal cycle" has no values for engine_capacity_cc for obvious reasons
     # assume average cyclist can push 100W ≈ 0.13 horsepower -> horsepower of standard car ~200 -> cyclist horsepower 0.2/200=0.00065 of a car -> set engine_capacity_cc of "Pedal cycle" to 0.00125 that of a car
-    engine_capacity_car = df.groupby("vehicle_type")["engine_capacity_cc"].median().loc["Car"]
-    df["engine_capacity_cc"] = np.where(df["vehicle_type"] == "Pedal cycle", 0.00065 * engine_capacity_car, df["engine_capacity_cc"])
+    if 'propulsion_code' in df.columns:
+        engine_capacity_car = df.groupby("vehicle_type")["engine_capacity_cc"].median().loc["Car"]
+        df["engine_capacity_cc"] = np.where(df["vehicle_type"] == "Pedal cycle", 0.00065 * engine_capacity_car, df["engine_capacity_cc"])
 
     df = df.drop(columns=["vehicle_type_2"])
 
     # check there are no more missing values
     assert sum(((df.isna()) | (df == "Missing")).any()) == 0
 
-    # creating date and time features
-    df['time_period'] = (df.time.str.slice(start=0, stop=2).astype('int') // 4)
-    df['time_period'] = (df['time_period'] * 4).astype('str') + ':00 - ' + ((df['time_period'] + 1) * 4).astype('str') + ':00'
-
-    df['date'] = pd.to_datetime(df.date)
-    df['month'] = df['date'].dt.month
-    df['season'] = [
-        'spring' if month in [3, 4, 5]
-        else 'summer' if month in [6, 7, 8]
-        else 'autumn' if month in [9, 10, 11]
-        else 'winter'
-        for month
-        in df['month']
-    ]
-
-    df = df.drop(columns=['date', 'month'])
-
     # select features
-    target = 'casualty_severity'
-    if target not in select_features:
-        select_features.append(target)
-    df = df[select_features]
+    df = df[[col for col in select_features if col not in drop_columns]]
 
     # apply transformations
     continuous_vars = [col for col in list(df.select_dtypes(exclude='object').columns) if col not in ['longitude', 'latitude']]
     scaler = MinMaxScaler()
     df[continuous_vars] = scaler.fit_transform(df[continuous_vars])
 
-    categorical_vars = [col for col in list(df.select_dtypes(include='object').columns) if col not in ['date', 'time', target]]
+    categorical_vars = [col for col in list(df.select_dtypes(include='object').columns) if col not in ['date', 'time', 'casualty_severity']]
     df = pd.get_dummies(df, columns=categorical_vars, drop_first=False)
+
+    # add empty columns for one-hot encoded columns that are missing
+    # sklearn requires that train and test data has the same columns
+    if encoded_cols_dict != None:
+        encoded_cols = []
+        for col in select_features:
+            if col not in drop_columns and col in encoded_cols_dict:
+                encoded_cols.extend(encoded_cols_dict[col])
+
+        missing_encoded_cols = [col for col in encoded_cols if col not in df.columns]
+        df[missing_encoded_cols] = np.zeros((len(df), len(missing_encoded_cols)))
+        df[missing_encoded_cols] = df[missing_encoded_cols].astype('bool')
+
+    # sklearn requires that train and test data has same column order
+    cols = sorted(list(df.columns))
+    df = df[cols]
 
     return df
